8b3df62016025e905a9a2e95fe6e64d7c2f5de8f
Kamil Brzozowski <idlesoluszyn@gmail.com>
2018-12-03 12:21:11 +0100

    commit

---
+++ app.js
@@ -0,0 +1,54 @@
+const home = document.querySelector('#home');
+const about = document.querySelector('#about');
+const services = document.querySelector('#services');
+const gallery = document.querySelector('#gallery');
+const blog = document.querySelector('#blog');
+const contact = document.querySelector('#contact');
+
+const homeNav = document.querySelector('#homeNav');
+const aboutNav = document.querySelector('#aboutNav');
+const servicesNav = document.querySelector('#servicesNav');
+const galleryNav = document.querySelector('#galleryNav');
+const blogNav = document.querySelector('#blogNav');
+const contactNav = document.querySelector('#contactNav');
+
+console.log(gallery)
+console.log(gallery.offsetTop);
+window.onscroll = () => {
+    if(window.scrollY + 100 >= home.offsetTop && window.scrollY + 100 <= (home.offsetTop + home.offsetHeight)) {
+        homeNav.style.borderTop = '2px solid orangered';
+    } else {
+        homeNav.style.borderTop = 'none';
+    }
+
+    if(window.scrollY + 100 >= about.offsetTop && window.scrollY + 100 <= (about.offsetTop + about.offsetHeight)) {
+        aboutNav.style.borderTop = '2px solid orangered';
+    } else {
+        aboutNav.style.borderTop = 'none';
+    }
+
+    if(window.scrollY + 100 >= services.offsetTop && window.scrollY + 100 <= (services.offsetTop + services.offsetHeight)) {
+        servicesNav.style.borderTop = '2px solid orangered';
+    } else {
+        servicesNav.style.borderTop = 'none';
+    }
+
+    if(window.scrollY + 100 >= gallery.offsetTop && window.scrollY + 100 <= (gallery.offsetTop + gallery.offsetHeight)) {
+        galleryNav.style.borderTop = '2px solid orangered';
+    } else {
+        galleryNav.style.borderTop = 'none';
+    }
+
+    if(window.scrollY + 100 >= blog.offsetTop && window.scrollY + 100 <= (blog.offsetTop + blog.offsetHeight)) {
+        blogNav.style.borderTop = '2px solid orangered';
+    } else {
+        blogNav.style.borderTop = 'none';
+    }
+
+    if(window.scrollY + 100 >= contact.offsetTop && window.scrollY + 100 <= (contact.offsetTop + contact.offsetHeight)) {
+        contactNav.style.borderTop = '2px solid orangered';
+    } else {
+        contactNav.style.borderTop = 'none';
+    }
+
+}
